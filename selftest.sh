18627d484ee8eac0d60f1adc7cf2c65034c5b9d6
doheyeopsong <vlsms119@wku.ac.kr>
2026-09-22 06:45:31 +0900

    selftest: URL-encode slug test vector; 20/20 green

--- selftest.sh
+++ selftest.sh
@@ -30,4 +30,4 @@ check openapi     "/openapi.json"
 check unknown-route "/nope" 404
 echo "PASS: $PASS  FAIL: $FAIL"
 [ "$FAIL" = "0" ]
-check slug  "/slug?text=Héllo Wörld! 10+ Great Ideas" 200
+check slug  "/slug?text=H%C3%A9llo+W%C3%B6rld!+10%2B+Great+Ideas" 200
