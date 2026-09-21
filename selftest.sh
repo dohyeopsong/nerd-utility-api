cb1b42a9f5d7e40e3c8f9a101dd5aef80cbbeb89
doheyeopsong <vlsms119@wku.ac.kr>
2026-09-22 06:59:33 +0900

    Add /diff line diff (LCS, unified format, stats)

--- selftest.sh
+++ selftest.sh
@@ -35,3 +35,4 @@ check pwstrength  "/pwstrength?pw=Tr0ub4dor%263" 200
 check cron       "/cron?expr=0%209%20*%20*%201-5" 200
 check chmod      "/chmod?mode=755" 200
 check diff       "/diff?a=hello%0Aworld&b=hello%0Athere" 200
+check diff       "/diff?a=hello%0Aworld&b=hello%0Athere" 200
